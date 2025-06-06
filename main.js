8d2f90c427523a8e73bda28c4245413cd328af65
Rajaabpro <rajaab.pro@gmail.com>
2025-06-06 15:50:47 +0500

    Add console log and initialize table body in main.js: include a greeting message and set up the table body element for future functionality.

--- main.js
+++ main.js
@@ -1,1 +1,3 @@
+console.log("Hello World");
 URL ="https://api.currencyapi.com/v3/latest?apikey=cur_live_hWw44oefgzPVles5WTTFIbprP8ljfXzXpo8Fgu2u"
+const tablebody = document.getElementsByTagName("tbody");
