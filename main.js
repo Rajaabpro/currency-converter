3e428e33e7f205f407002d7e8296432dd0b7d8aa
Rajaabpro <rajaab.pro@gmail.com>
2025-06-07 00:38:45 +0500

    Add table row to main.js: populate the table body with sample data for initial display, enhancing the user interface for future currency data integration.

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 console.log("Hello World");
 URL ="https://api.currencyapi.com/v3/latest?apikey=cur_live_hWw44oefgzPVles5WTTFIbprP8ljfXzXpo8Fgu2u"
 const tablebody = document.getElementsByTagName("tbody");
+tablebody.innerHTML = `
+<tr>
+    <td>data 1</td>
+    <td>data 2</td>
+    <td>data 3</td>
+</tr>
+`
