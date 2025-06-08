cef9868d65dec849ca6a0f19b5c0e551312a2d58
Rajaabpro <rajaab.pro@gmail.com>
2025-06-08 12:20:01 +0500

    Add button click event listener in main.js: implement functionality to log a message when the button is clicked, enhancing interactivity for user engagement.

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 console.log("Hello World");
 URL ="https://api.currencyapi.com/v3/latest?apikey=cur_live_hWw44oefgzPVles5WTTFIbprP8ljfXzXpo8Fgu2u"
+
+const btn = document.querySelector("btn");
+btn.addEventListener("click", (e) => {
+    e.preventDefault
+    console.log("Button clicked");
+});
 const tablebody = document.getElementsByTagName("tbody");
 tablebody.innerHTML = `
 <tr>
